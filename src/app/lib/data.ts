f5158edf691e4b2f9ee92e3517883a010cb844cf
gracekarinn <gracekarina12@gmail.com>
2024-07-05 14:21:13 +0700

    feat: Add navigation links to navbar component

---
+++ src/app/lib/data.ts
@@ -0,0 +1,13 @@
+import { SiAboutdotme } from "react-icons/si";
+import { MdOutlineWorkHistory } from "react-icons/md";
+import { MdOutlineCastForEducation } from "react-icons/md";
+import { IoLanguage } from "react-icons/io5";
+import { MdContactEmergency } from "react-icons/md";
+
+export const navLinks = [
+    { href: "/", icon: SiAboutdotme },
+    { href: "/", icon: MdOutlineCastForEducation },
+    { href: "/", icon: MdOutlineWorkHistory },
+    { href: "/", icon: IoLanguage },
+    { href: "/", icon: MdContactEmergency }
+];
