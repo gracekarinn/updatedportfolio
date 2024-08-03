9df8b3b0123915dcb6090d754347cc45c98075ed
gracekarinn <gracekarina12@gmail.com>
2024-08-04 01:15:48 +0700

    feat: add new data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,20 +4,75 @@ import { MdOutlineCastForEducation } from "react-icons/md";
 import { IoLanguage } from "react-icons/io5";
 import { MdContactEmergency } from "react-icons/md";
 import { TimelineItemType } from "./interface";
+import { Skill } from "./interface";
 
 export const navLinks = [
-  { href: "/", icon: SiAboutdotme },
-  { href: "/education", icon: MdOutlineCastForEducation },
-  { href: "/", icon: MdOutlineWorkHistory },
-  { href: "/", icon: IoLanguage },
-  { href: "/", icon: MdContactEmergency },
+  { href: "#about", icon: SiAboutdotme, name: "About" },
+  { href: "#education", icon: MdOutlineCastForEducation },
+  { href: "#experience", icon: MdOutlineWorkHistory },
+  { href: "#language", icon: IoLanguage },
+  { href: "#contact", icon: MdContactEmergency },
 ];
 
 export const deskripsi =
   "Coffee lover, rubik's cube enthusiast, and coding explorer. Welcome, Fellow Coders! Let's connect and collaborate ^__^";
 
 export const timelineData: TimelineItemType[] = [
-  { year: 2020, title: "Event 1", description: "Description of event 1" },
-  { year: 2021, title: "Event 2", description: "Description of event 2" },
-  { year: 2021, title: "Event 2", description: "Description of event 2" },
+  {
+    year: 2023,
+    title: "Computer Science",
+    description: "Universitas Indonesia",
+  },
+  {
+    year: 2022,
+    title: "Pharmaceutical Science and Technology",
+    description: "Institut Teknologi Bandung",
+  },
+  {
+    year: 2019,
+    title: "SMAN 68 Jakarta",
+    description: "Located in Jakarta Pusat",
+  },
+];
+
+export const projectsData = [
+  {
+    title: "SBF Project",
+    description:
+      "I contributed to the development of a website for a BEM project.",
+    tags: ["Next.js", "Tailwind"],
+    imageUrl: "/sbf.png",
+  },
+  {
+    title: "SBF Project",
+    description:
+      "I contributed to the development of a website for a BEM project.",
+    tags: ["Next.js", "Tailwind"],
+    imageUrl: "/sbf.png",
+  },
+  {
+    title: "SBF Project",
+    description:
+      "I contributed to the development of a website for a BEM project.",
+    tags: ["Next.js", "Tailwind"],
+    imageUrl: "/sbf.png",
+  },
+  {
+    title: "SBF Project",
+    description:
+      "I contributed to the development of a website for a BEM project.",
+    tags: ["Next.js", "Tailwind"],
+    imageUrl: "/sbf.png",
+  },
+] as const;
+
+export const skills: Skill[] = [
+  { title: "HTML", percent: 70 },
+  { title: "CSS", percent: 70 },
+  { title: "Tailwind CSS", percent: 80 },
+  { title: "JavaScript", percent: 60 },
+  { title: "TypeScript", percent: 70 },
+  { title: "NestJs", percent: 40 },
+  { title: "Python", percent: 75 },
+  { title: "Java", percent: 70 },
 ];
