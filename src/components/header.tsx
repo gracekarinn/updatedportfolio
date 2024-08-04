94848bc4422cf129b08be591f1014af15155ab33
gracekarinn <gracekarina12@gmail.com>
2024-08-04 15:55:33 +0700

    feat: add dark and light icon

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,30 +1,34 @@
 "use client";
-import React, { useState } from "react";
-import { FaToggleOff, FaToggleOn } from "react-icons/fa6";
+
+import React from "react";
+import { useTheme } from "./theme-switch";
+import { CiDark, CiLight } from "react-icons/ci";
 
 const Header = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <header className="container bg-white bg-opacity-30 fixed top-0 left-0 right-0 z-50">
+    <header className="container fixed top-0 left-0 right-0 z-50 bg-white bg-opacity-30 dark:bg-gray-800 dark:bg-opacity-30 transition-colors duration-300">
       <div className="container mx-auto my-3">
         <div className="flex justify-between items-center">
-          <a href="#" className="text-2xl px-2 font-bold text-black">
+          <a
+            href="#"
+            className="text-2xl px-2 font-bold text-black dark:text-white transition-colors duration-300"
+          >
             Grace <span className="text-purple-400">Karin</span>
           </a>
           <div className="flex">
             <button
-              className="mx-1 text-black px-4 py-2 flex items-center"
-              onClick={toggleDarkMode}
+              className="p-3 mx-3 rounded-full bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-purple-400 dark:focus:ring-purple-500 shadow-md hover:shadow-lg hover:scale-110"
+              onClick={toggleTheme}
+              aria-label={`Switch to ${
+                theme === "light" ? "dark" : "light"
+              } mode`}
             >
-              {isDarkMode ? (
-                <FaToggleOn style={{ fontSize: "48px" }} />
+              {theme === "light" ? (
+                <CiDark className="w-5 h-5 text-gray-800" />
               ) : (
-                <FaToggleOff style={{ fontSize: "48px" }} />
+                <CiLight className="w-5 h-5 text-yellow-300" />
               )}
             </button>
           </div>
